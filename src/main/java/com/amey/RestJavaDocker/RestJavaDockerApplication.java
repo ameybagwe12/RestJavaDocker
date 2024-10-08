81edf9de7898b54e6fcf5bf71dba78497ffd93f6
Amey Bagwe <ameybagwe09@gmail.com>
2024-10-08 12:42:37 +0530

    Docker with Spring

---
+++ src/main/java/com/amey/RestJavaDocker/RestJavaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.amey.RestJavaDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJavaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJavaDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amey/RestJavaDocker/RestJavaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amey.RestJavaDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJavaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
